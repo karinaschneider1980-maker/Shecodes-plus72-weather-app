39580a064cfe8f2e2a6446beeecbfe06ef3396f2
karinaschneider1980-maker <karinaschneider1980@gmail.com>
2026-03-07 19:25:01 +0100

    updated HTML JS and CSS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,54 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature")
+  let temperature = response.data.temperature.current
+  let cityElement = document.querySelector("#city")
+  let descriptionElement = document.querySelector("#description")
+  let humidityElement = document.querySelector("#humidity")
+  let windElement = document.querySelector("#wind")
+  let timeElement = document.querySelector("#time")
+  let date = new Date(response.data.time * 1000)
+  let iconElement = document.querySelector("#icon")
+
+  cityElement.innerHTML = response.data.city
+  timeElement.innerHTML = formatDate(date)
+  descriptionElement.innerHTML = response.data.condition.description
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`
+  windElement.innerHTML = `${response.data.wind.speed}km/h`
+  temperatureElement.innerHTML = Math.round(temperature)
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes()
+  let hours = date.getHours()
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ]
+  let day = days[date.getDay()]
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`
+  }
+
+  return `${day} ${hours}:${minutes}`
+}
+
+function searchForCity(city) {
+  let apiKey = "d37caee084ftd6c3baf32ae334oca1bf"
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`
+  axios.get(apiUrl).then(refreshWeather)
+}
+
 function searchCity(event) {
   event.preventDefault()
   let searchInput = document.querySelector("#search-form-input")
-  let cityElement = document.querySelector("#city")
-  cityElement.innerHTML = searchInput.value
+  searchForCity(searchInput.value)
 }
 
 let searchFormElement = document.querySelector("#search-form")
